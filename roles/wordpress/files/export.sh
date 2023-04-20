3be808d3336e7a9b96ca1ad6d8bea142f271c5e7
Markus K Brechtel <markus.brechtel@uk-koeln.de>
2023-04-20 07:39:49 +0200

    add wordpress migration tasks

--- roles/wordpress/files/export.sh
+++ roles/wordpress/files/export.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker-compose run --rm -T db sh -c 'exec mysqldump --user=root --password=$MYSQL_ROOT_PASSWORD --host=db $MYSQL_DATABASE' | gzip > export/db.sql.gz
-docker-compose run --rm -u root -T wordpress-cli tar cz . > export/wordpress.tar.gz
+# fail on errors
+set -e
+
+docker-compose run --rm -v $(pwd)/export/:/mnt/export/ db sh -c 'mysqldump --user=root --password=$MYSQL_ROOT_PASSWORD --host=db $MYSQL_DATABASE | gzip > /mnt/export/db.sql.gz'
+docker-compose run --rm -u root -v $(pwd)/export/:/mnt/export/ wordpress-cli tar cz --exclude=debug.log  -f /mnt/export/wordpress.tar.gz .
