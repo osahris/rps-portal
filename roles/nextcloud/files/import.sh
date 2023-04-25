5585c6e6f2303623af4964892925758aa522fc87
Markus K Brechtel <markus.brechtel@uk-koeln.de>
2023-04-25 16:23:47 +0200

    further improvements to nextcloud import

--- roles/nextcloud/files/import.sh
+++ roles/nextcloud/files/import.sh
@@ -11,9 +11,11 @@ cp ./import/nextclouddb.sql ./initdb/
 
 # Import the Nextcloud folder
 docker-compose run --rm -u root --no-deps -v $(pwd)/import/nextcloud/:/mnt/import/nextcloud/:ro nextcloud /usr/bin/rsync -avrzP --sparse /mnt/import/nextcloud/ /var/www/html
+docker-compose run --rm -u root --no-deps nextcloud chown -R www-data:www-data /var/www/html
 
 # Import the Nextcloud data
 docker-compose run --rm -u root --no-deps -v $(pwd)/import/nextcloud-data/:/mnt/import/nextcloud-data/:ro nextcloud /usr/bin/rsync -avrzP --sparse /mnt/import/nextcloud-data/ /srv/nextcloud/data
+docker-compose run --rm -u root --no-deps nextcloud chown -R www-data:www-data /srv/nextcloud/data
 
 # Set the Nextcloud config for database
 docker-compose run -T --rm -u www-data --no-deps nextcloud php < update-config.php
