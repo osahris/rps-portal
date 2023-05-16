2747d1679321d6af8315c21238a830ca8fde1803
Markus K Brechtel <markus.brechtel@uk-koeln.de>
2023-05-16 16:34:11 +0200

    add nextcloud export commands to script

--- roles/nextcloud/files/export.sh
+++ roles/nextcloud/files/export.sh
@@ -1,1 +1,9 @@
-# docker-compose run --rm -v $(pwd)/export:/mnt/export postgres pg_dump -h postgres -U nextcloud -f /mnt/export/nextclouddb.sql nextcloud
+
+# database
+docker-compose run --rm -v $(pwd)/export/:/mnt/export/ postgres pg_dump -h postgres -U nextcloud -f /mnt/export/nextclouddb.sql nextcloud
+
+# Export the Nextcloud folder
+docker-compose run --rm -u root --no-deps -v $(pwd)/export/nextcloud/:/mnt/export/nextcloud/ nextcloud /usr/bin/rsync -avrzP --sparse /var/www/html/ /mnt/export/nextcloud/
+
+# Export the Nextcloud data
+docker-compose run --rm -u root --no-deps -v $(pwd)/export/nextcloud-data/:/mnt/export/nextcloud-data/ nextcloud /usr/bin/rsync -avrzP --sparse /srv/nextcloud/data/ /mnt/export/nextcloud-data/
