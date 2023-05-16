e4b6125acd84dc7a1b0a1d032a46484878fa7f88
Markus K Brechtel <markus.brechtel@uk-koeln.de>
2023-05-16 18:11:07 +0200

    export nextcloud with postgres user

--- roles/nextcloud/files/export.sh
+++ roles/nextcloud/files/export.sh
@@ -1,6 +1,6 @@
 
 # database
-docker-compose run --rm -v $(pwd)/export/:/mnt/export/ postgres pg_dump -h postgres -U nextcloud -f /mnt/export/nextclouddb.sql nextcloud
+docker-compose exec -T -u postgres postgres pg_dump > export/nextclouddb.sql || rm export/nextclouddb.sql
 
 # Export the Nextcloud folder
 docker-compose run --rm -u root --no-deps -v $(pwd)/export/nextcloud/:/mnt/export/nextcloud/ nextcloud /usr/bin/rsync -avrzP --sparse /var/www/html/ /mnt/export/nextcloud/
