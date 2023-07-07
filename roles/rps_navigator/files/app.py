0a360cb5a7375664e756cbc54fe54204e42e905b
tulskyva <vasilii.tulskii@kgu.de>
2023-07-07 15:39:29 +0200

    Introduce current status for each service on navigation page

--- roles/rps_navigator/files/app.py
+++ roles/rps_navigator/files/app.py
@@ -2,6 +2,7 @@
 # Tulsky V.A. 2023 #
 ####################
 from flask import Flask, render_template, request, redirect
+import requests
 import os
 
 app = Flask(__name__)
@@ -10,30 +11,43 @@ app = Flask(__name__)
 def home():
     title = "RPS Services Navigator"
     domain = os.environ.get("HOST_DOMAIN")
-    upperdomain = '.'.join(domain.split('.')[1:])
+    # upperdomain = '.'.join(domain.split('.')[1:])
     services = [{'name': 'Budibase',                'subdomain': 'base'},
                 {'name': 'Gitea',                   'subdomain': 'code'},
                 {'name': 'Header',                  'subdomain': 'header'},
-                # {'name': 'Header (static)',         'subdomain': 'static-header-test'},
+                {'name': 'Header (static)',         'subdomain': 'static-header-test'},
                 {'name': 'Keycloak',                'subdomain': 'accounts'},
-                {'name': 'rps_people',              'subdomain': 'people'},
                 {'name': 'MatrixChat',              'subdomain': 'element.chat'},
                 {'name': 'MVP',                     'subdomain': 'mvp'},
                 {'name': 'Nextcloud',               'subdomain': 'cloud'},
-                # {'name': 'OpenProject',             'subdomain': 'openproject'},
-                # {'name': 'RocketChat',              'subdomain': 'rocketchat'},
+                {'name': 'OpenProject',             'subdomain': 'openproject'},
+                {'name': 'RocketChat',              'subdomain': 'rocketchat'},
                 {'name': 'rps_admin_interface',     'subdomain': 'admin'},
-                {'name': 'rps_navigator',           'subdomain': 'navigator'}, 
+                # {'name': 'rps_navigator',           'subdomain': 'navigator'}, 
+                {'name': 'rps_people',              'subdomain': 'people'},
                 {'name': 'rps_groups_interface',    'subdomain': 'groups',       'path': 'realms/rps'},
                 {'name': 'Traefik',                 'subdomain': 'traefik'},
                 {'name': 'Wiki Bookstack',          'subdomain': 'wiki'},
-                # {'name': 'Wiki.js',                 'subdomain': 'wiki-js'},
-                # {'name': 'Wordpress (Draft)',       'subdomain': 'www-draft'},
-                # {'name': 'Wordpress (Live)',        'subdomain': 'www'},
+                {'name': 'Wiki.js',                 'subdomain': 'wiki-js'},
+                {'name': 'Wordpress (Draft)',       'subdomain': 'www-draft'},
+                {'name': 'Wordpress (Live)',        'subdomain': 'www'},
                 ]
+    for service in services:
+        url_protocol = 'https'
+        url =  url_protocol + "://"
+        if 'subdomain' in service:
+            url += service['subdomain'] + '.'
+        url += domain 
+        if 'path' in service:
+            url += '/' + service['path']
+        try:
+            status_code = requests.head(url,timeout=1).status_code
+        except:
+            status_code = '404'
+        service['received_code'] = status_code
     return  render_template('index.html', 
                             title = title,
-                            domain = upperdomain,
+                            domain = domain,
                             services = services)
 
 @app.errorhandler(404)
